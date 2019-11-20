f662f605521e8c25c68bd5d17b58fa0c9beac3ab
zhangl <zhangl@startimes.com.cn>
2019-11-20 18:30:51 +0800

    add quic session general info

--- test/quic_connection_json_loader.py
+++ test/quic_connection_json_loader.py
@@ -4,6 +4,7 @@ if __name__ == '__main__':
     file_path = "../data_converted/quic-gh2ir_quic_connection.json"
     with open(file_path, 'r') as load_f:
         load_dict = json.load(load_f)
+        print('load general info: ', len(load_dict['general_info']))
         print('load packet_sent: ', len(load_dict['packets_sent']))
         print('load packet_received: ', len(load_dict['packets_received']))
         print('load stream_dict: ', len(load_dict['stream_dict']))
