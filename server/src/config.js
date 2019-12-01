2106f95b967b10990f273921e7a81f8f8180ec3e
dev135208 <dev135208@qq.com>
2019-12-01 19:06:21 +0800

    修改服务器端口

--- server/src/config.js
+++ server/src/config.js
@@ -2,5 +2,5 @@ module.exports = {
   htmlStatic: '../../../html/', // ecahrts图形化界面所在位置
   pythonStatic: '../../../resource/', // python图像化数据所在位置
   shellStatic: '../../src/cronet_log_analyze.py', // python命令所在位置
-  port: 8888,
+  port: 80,
 }
