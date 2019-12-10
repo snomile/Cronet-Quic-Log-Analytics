ddaf21a644991e51650898a0950183c7f68d931f
yangxl <327976201@qq.com>
2019-12-10 11:32:43 +0800

    add client ip print

--- server/src/util.js
+++ server/src/util.js
@@ -24,7 +24,9 @@ const error = (msg, color = 'orangered ') => {
 
 const getClientIP = (req) => {
   return req.headers['x-forwarded-for'] || // 判断是否有反向代理 IP
-    req.headers['x-real-ip'];
+    req.connection.remoteAddress || // 判断 connection 的远程 IP
+    req.socket.remoteAddress || // 判断后端的 socket 的 IP
+    req.connection.socket.remoteAddress;
 };
 
 module.exports = {
