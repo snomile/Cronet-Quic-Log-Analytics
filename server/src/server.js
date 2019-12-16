3b6b5b7fd379ec2af1b2040a5bb743d1c6fb2178
yangxl <327976201@qq.com>
2019-12-16 17:12:31 +0800

    Catch unknown upload exceptions and return to the client

--- server/src/server.js
+++ server/src/server.js
@@ -33,78 +33,82 @@ app.use(staticFiles(path.join(__dirname + '/upload/')))
 
 // 上传服务
 router.post('/upload', async (ctx, next) => {
-  const file = ctx.request.files.file; // 上传的文件在ctx.request.files.file
-  const clientIp = getClientIP(ctx.req).replace('::ffff:', '');
-  const userAgent = ctx.request.header['user-agent'].replace(/\s|\/|\.|\*|\(|\)|,|;/igm, '_');
-  log(`upload file begin: ${JSON.stringify(file)}`);
-  if (file.type.indexOf('zip') < 0 && file.type.indexOf('json') < 0) {
-    const errorMsg = `upload error: the file type is ${file.type}, accept: json,zip`;
-    log(errorMsg,'red');
-    ctx.status = 400;
-    return ctx.body = { code: 400, message: errorMsg };
-  }
-  // 修改文件的名称
-  var day = dayjs().format('YYYY-MM-DD');
-  var dayPath = path.join(__dirname, '/upload/', day);
-  var hasPath = true;
   try {
-    hasPath = fs.existsSync(dayPath);
-  } catch(e) {
-    hasPath = false;
-  }
-  if (!hasPath) {
-    fs.mkdirSync(dayPath);
-  }
-  var extName = dayjs().format('YYYY.MM.DD_HH:mm:ss') + '-' + clientIp + '-' + userAgent + '-';
-  var newFileName = '/' + extName + file.name;
-  var targetPath = dayPath + newFileName;
-  // 写入本身是异步的，这里改为同步方法，防止接下来的执行报错
-  var writeFile = function () {
-    return new Promise(function (resolve, reject) {
-      // 创建可读流
-      const reader = fs.createReadStream(file.path);
-      //创建可写流
-      const upStream = fs.createWriteStream(targetPath);
-      // 可读流通过管道写入可写流
-      reader.pipe(upStream);
-      upStream.on('finish', () => {
-        resolve('finish');
+    const file = ctx.request.files.file; // 上传的文件在ctx.request.files.file
+    const clientIp = getClientIP(ctx.req).replace('::ffff:', '');
+    const userAgent = ctx.request.header['user-agent'].replace(/\s|\/|\.|\*|\(|\)|,|;/igm, '_');
+    log(`upload file begin: ${JSON.stringify(file)}`);
+    if (file.type.indexOf('zip') < 0 && file.type.indexOf('json') < 0) {
+      const errorMsg = `upload error: the file type is ${file.type}, accept: json,zip`;
+      log(errorMsg, 'red');
+      ctx.status = 400;
+      return ctx.body = { code: 400, message: errorMsg };
+    }
+    // 修改文件的名称
+    var day = dayjs().format('YYYY-MM-DD');
+    var dayPath = path.join(__dirname, '/upload/', day);
+    var hasPath = true;
+    try {
+      hasPath = fs.existsSync(dayPath);
+    } catch (e) {
+      hasPath = false;
+    }
+    if (!hasPath) {
+      fs.mkdirSync(dayPath);
+    }
+    var extName = dayjs().format('YYYY.MM.DD_HH:mm:ss') + '-' + clientIp + '-' + userAgent + '-';
+    var newFileName = '/' + extName + file.name;
+    var targetPath = dayPath + newFileName;
+    // 写入本身是异步的，这里改为同步方法，防止接下来的执行报错
+    var writeFile = function () {
+      return new Promise(function (resolve, reject) {
+        // 创建可读流
+        const reader = fs.createReadStream(file.path);
+        //创建可写流
+        const upStream = fs.createWriteStream(targetPath);
+        // 可读流通过管道写入可写流
+        reader.pipe(upStream);
+        upStream.on('finish', () => {
+          resolve('finish');
+        });
       });
-    });
-  }
-  await writeFile();
-  if (file.type.indexOf('zip') >= 0) {
-    const shell = `unzip -o ${targetPath} -d ${dayPath}/`;
-    log(`upload a zip file, begin unzip the file:${shell}`);
-    let zipFileName = '';
-    let lastFileName = ''
-    const res = shelljs.exec(shell);
-    if (res.stdout) {
-      const resLog = res.stdout;
-      resLog.split('\n').forEach(line => {
-        if (line.indexOf('inflating:') >= 0) {
-          zipFileName = line.replace('inflating:', '').trim();
+    }
+    await writeFile();
+    if (file.type.indexOf('zip') >= 0) {
+      const shell = `unzip -o ${targetPath} -d ${dayPath}/`;
+      log(`upload a zip file, begin unzip the file:${shell}`);
+      let zipFileName = '';
+      let lastFileName = ''
+      const res = shelljs.exec(shell);
+      if (res.stdout) {
+        const resLog = res.stdout;
+        resLog.split('\n').forEach(line => {
+          if (line.indexOf('inflating:') >= 0) {
+            zipFileName = line.replace('inflating:', '').trim();
+          }
+        })
+        if (zipFileName) {
+          lastFileName = `${zipFileName.replace(`${dayPath}/`, `${dayPath}/${extName}`)}`;
+          shelljs.exec(`mv ${zipFileName} ${lastFileName}`);
+        } else {
+          log(res.stderr, 'red');
+          ctx.status = 400;
+          return ctx.body = { code: 400, message: res.stdout, error: res.stderr };
         }
-      })
-      if (zipFileName) {
-        lastFileName = `${zipFileName.replace(`${dayPath}/`, `${dayPath}/${extName}`)}`;
-        shelljs.exec(`mv ${zipFileName} ${lastFileName}`);
       } else {
         log(res.stderr, 'red');
         ctx.status = 400;
         return ctx.body = { code: 400, message: res.stdout, error: res.stderr };
       }
-    } else {
-      log(res.stderr, 'red');
-      ctx.status = 400;
-      return ctx.body = { code: 400, message: res.stdout, error: res.stderr };
-    }
-    log('unzip done, delete the zip file');
-    shelljs.exec('rm ' + dayPath + '/*.zip');
+      log('unzip done, delete the zip file');
+      shelljs.exec('rm ' + dayPath + '/*.zip');
 
-    return ctx.body = { code: 200, data: { url: '/' + lastFileName.substring(lastFileName.indexOf(day), lastFileName.length), local: lastFileName } };
-  } else {
-    return ctx.body = { code: 200, data: { url: '/' + day + newFileName, local: targetPath } };
+      return ctx.body = { code: 200, data: { url: '/' + lastFileName.substring(lastFileName.indexOf(day), lastFileName.length), local: lastFileName } };
+    } else {
+      return ctx.body = { code: 200, data: { url: '/' + day + newFileName, local: targetPath } };
+    }
+  } catch(e) {
+    return ctx.body = { code: 400, error: e.toString() };
   }
 });
 
