6e0627c81925cc4f71f37afe4a3196021f046421
zhangl <zhangl@startimes.com.cn>
2019-12-27 16:32:46 +0800

    save general info to json files

--- src/cronet_log_analyze.py
+++ src/cronet_log_analyze.py
@@ -1,20 +1,22 @@
+import json
 import os
 import sys
 import argparse
+import time
 
 from process import cronet_log_loader,cronet_session
 from visualize import helper_data, helper_graph
 from visualize import graph
 
-def check_key_path(project_root):
+def check_key_path(project_root,output_path):
     key_paths = [project_root + '/resource/html_output',
                  project_root + '/resource/data_original',
-                 project_root + '/resource/data_converted'
+                 project_root + '/resource/data_converted',
+                 output_path
                  ]
     for key_path in key_paths:
         if not os.path.exists(key_path):
             os.makedirs(key_path)
-
 
 def find_usable_input_path(file_path):
     cur_abs_path = os.path.abspath(os.curdir)
@@ -24,8 +26,6 @@ def find_usable_input_path(file_path):
     usable_input_path = None
     for usable_input_path in paths:
         if os.path.exists(usable_input_path):
-            print('--------------------------------')
-            print(usable_input_path)
             break
     return usable_input_path
 
@@ -45,8 +45,26 @@ def process_show(usable_input_path,args):
         helper_data.init(json_file)
         json_file_name_without_ext = json_file[json_file.rfind('/') + 1: -21]
         host_starttime = json_file_name_without_ext.replace(filename_without_ext + '_', '')
-        helper_graph.init(project_root, filename_without_ext, host_starttime)
+        helper_graph.init(args.output_path, filename_without_ext, host_starttime)
         graph.show(args.show_all_packet_info,args.show_receive_send, args.show_ack_delay, args.show_size_inflight)
+
+
+
+
+def generate_event_session_result(output_path):
+    files = os.listdir(output_path)
+    event_session_dict = {}
+    for file in files:
+        if file.endswith('_general_info.json'):
+            event_session_name = file[:file.index('_general_info.json')]
+            event_session_files = [event_session_file for event_session_file in files if event_session_file.startswith(event_session_name)]
+            event_session_dict[event_session_name]=event_session_files
+
+    event_session_info_name = output_path + 'event_session_info.json'
+    with open(event_session_info_name, "w") as f:
+        json.dump(event_session_dict, f)
+
+    print("generate event session info:", event_session_info_name)
 
 
 if __name__ == '__main__':
@@ -55,8 +73,8 @@ if __name__ == '__main__':
     project_root = abs_program_path[:abs_program_path.index('/src/cronet_log_analyze.py')]
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("--log_path", help="absloute path of the log file", default = '/Users/zhangliang/PycharmProjects/chrome_quic_log_analytics/resource/data_original/netlog-1575253082.json')
-    parser.add_argument("--output_path", help="absloute path of the output files", default=project_root + "/resource/data_converted/")
+    parser.add_argument("--log_path", help="absloute path of the log file", default = 'some_file_name2.json')
+    parser.add_argument("--output_path", help="absloute path of the output files", default= "%s/resource/data_converted/%s/" % (project_root, time.time()))
     parser.add_argument("--show_all_packet_info", help="show_all_packet_info", default=True)
     parser.add_argument("--show_receive_send", help="show_receive_send", default=True)
     parser.add_argument("--show_ack_delay", help="show_ack_delay", default=False)
@@ -67,10 +85,11 @@ if __name__ == '__main__':
     cronet_session.IGNORE_DOMAIN_NAME_LIST = args.ignore.split(';')
     print(args)
 
-    #process
-    check_key_path(project_root)
+    #process data
+    check_key_path(project_root,args.output_path)
     usable_input_path = find_usable_input_path(args.log_path)
     if usable_input_path:
         process_show(usable_input_path,args)
+        generate_event_session_result(args.output_path)
     else:
         print('file %s does not exist, exit now' % usable_input_path)
--- src/visualize/helper_graph.py
+++ src/visualize/helper_graph.py
@@ -3,13 +3,13 @@ from bokeh.plotting import figure, show, save, output_file
 import warnings
 warnings.filterwarnings('ignore')
 
-project_root = None
+html_output_path = None
 filename = None
 host = None
 
-def init(root_path, filename_without_ext,host_starttime):
-    global project_root,filename,host
-    project_root = root_path
+def init(output_path, filename_without_ext,host_starttime):
+    global html_output_path,filename,host
+    html_output_path = output_path
     filename = filename_without_ext
     host = host_starttime
 
@@ -19,14 +19,15 @@ def get_plot(x_label,y_label,title):
                tools='wheel_zoom,xwheel_zoom, ywheel_zoom,box_zoom,hover,box_select,reset,pan,crosshair', active_scroll="wheel_zoom")
     p.xaxis.axis_label = x_label
     p.yaxis.axis_label = y_label
-    p.title.text = title + '_' + host
+    p.title.text = host + '_' + title
     return p
 
 def display(plot):
     plot.legend.location = "top_left"
     plot.legend.click_policy = "mute"
-    output_filename = datetime.datetime.now().strftime('%y%m%d_%H%M%S_') + filename.replace(':', '_') + '_' + plot.title.text.replace(" ", "").lower()
-    output_filepath = project_root + "/resource/html_output/%s.html" % output_filename
+    safe_filename = filename.replace(':','_')  # convert : to _ to avoid corrupting the file system
+    output_filename = safe_filename + '_' + plot.title.text.replace(" ", "").lower()
+    output_filepath = "%s/%s.html" % (html_output_path,output_filename)
     output_file(output_filepath)
     print('generate html at', output_filepath)
     show(plot)
