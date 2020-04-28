02ec2f2d02c67a9a5ac4ec354ff711ea29a93872
guol <guol@startimes.com.cn>
2020-04-28 16:31:40 +0800

    1.remove codes commented out 2.fix bug:   modify general_info file to quic_session start time

--- src/process/cronet_log_loader.py
+++ src/process/cronet_log_loader.py
@@ -5,8 +5,6 @@ from collections import OrderedDict
 
 from process import constant_converter, probe_common, probe_quic, probe_http2
 from process.netlog_event import NetlogEvent
-# from process.quic_session import QuicSession, ClientQuicSession, ServerQuicSession
-# from process.quic_connection import QuicConnection
 
 
 def get_formed_log_events(log_events):
@@ -41,72 +39,6 @@ def get_formed_log_events(log_events):
         elif "|SOCKET" in event_key:
             sockets[event_key] = value
     return url_requests, http_stream_job_controllers, http_stream_jobs, quic_sessions, http2_sessions, http_sessions, sockets, net_log_events.values()
-
-
-def process_url_request(formed_elem):
-    url_requests = []
-    for key, values in formed_elem.items():
-        key_elem = key.split("|")
-        url_request = {
-            "url_request_id": int(key_elem[0]),
-        }
-        original_data = {}
-        for event in values:
-            if original_data.get('probe_start_time_int') is None:
-                original_data['probe_start_time_int'] = event.time_int
-            if 'URL_REQUEST_START_JOB' == event.event_type and 'PHASE_BEGIN' == event.phase and original_data.get('url_request_start_job_time_int') is None:
-                original_data['url_request_start_job_time_int'] = event.time_int
-            if "HTTP_STREAM_REQUEST" == event.event_type and 'PHASE_END' == event.phase and original_data.get('first_http_stream_request_end_time_int') is None:
-                original_data['first_http_stream_request_end_time_int'] = event.time_int
-            if "HTTP_STREAM_REQUEST" == event.event_type and 'PHASE_BEGIN' == event.phase:
-                original_data['last_http_stream_request_begin_time_int'] = event.time_int
-            if "HTTP_TRANSACTION_SEND_REQUEST" == event.event_type and 'PHASE_BEGIN' == event.phase and original_data.get('first_http_transaction_send_request_begin_time_int') is None:
-                original_data['first_http_transaction_send_request_begin_time_int'] = event.time_int
-            if "HTTP_TRANSACTION_READ_HEADERS" == event.event_type and 'PHASE_BEGIN' == event.phase and original_data.get('first_http_transaction_read_headers_begin_time_int') is None:
-                original_data['first_http_transaction_read_headers_begin_time_int'] = event.time_int
-            if "HTTP_TRANSACTION_READ_RESPONSE_HEADERS" == event.event_type and 'PHASE_NONE' == event.phase and original_data.get('first_http_transaction_read_response_headers_protocol') is None:
-                original_data['first_http_transaction_read_response_headers_protocol'] = event.other_data['params']['headers'][0]
-            if "REQUEST_ALIVE" == event.event_type and 'PHASE_BEGIN' == event.phase and original_data.get('probe_url') is None:
-                original_data['probe_url'] = event.other_data['params']['url']
-            original_data['probe_end_time_int'] = event.time_int
-        # url_request['conn_establish_duration'] = abs(original_data.get('last_http_stream_request_begin_time_int', -1) - original_data.get('first_http_stream_request_end_time_int', -1))
-        # url_request['probe_request_start_offset'] = original_data.get('first_http_transaction_send_request_begin_time_int', -1) - original_data.get('probe_start_time_int', -1)
-        # url_request['first_pkg_offset'] = original_data.get('first_http_transaction_read_headers_begin_time_int', -1)- original_data.get('probe_start_time_int', -1)
-        # url_request['first_pkg_status'] = original_data.get('first_http_transaction_read_response_headers_protocol').split(" ")[1] if "HTTP" in original_data.get('first_http_transaction_read_response_headers_protocol', "NULL") else -1
-        # url_request['probe_start_delayed_duration'] = original_data.get('url_request_start_job_time_int', -1) - original_data.get('probe_start_time_int', -1)
-        url_request['probe_start_delayed_duration'] = original_data.get('url_request_start_job_time_int', -1) - original_data.get('probe_start_time_int', -1)
-        url_request['conn_establish_duration'] = original_data.get('last_http_stream_request_begin_time_int', -1) - original_data.get('url_request_start_job_time_int', -1)
-        url_request['probe_request_start_offset'] = original_data.get('first_http_transaction_send_request_begin_time_int', -1) - original_data.get('url_request_start_job_time_int', -1)
-        url_request['first_pkg_offset'] = original_data.get('first_http_transaction_read_headers_begin_time_int', -1) - original_data.get('url_request_start_job_time_int', -1)
-        url_request['first_pkg_status'] = int(original_data.get('first_http_transaction_read_response_headers_protocol').split(" ")[1]) if "HTTP" in original_data.get('first_http_transaction_read_response_headers_protocol', "NULL") else -1
-        url_request['probe_duration'] = original_data['probe_end_time_int'] - original_data.get('url_request_start_job_time_int', -1)
-        url_request['probe_url'] = original_data['probe_url']
-        url_request['original_data'] = original_data
-        url_requests.append(url_request)
-    return url_requests
-
-
-def process_http_stream_job_controller(formed_elem):
-    http_stream_job_controllers = []
-    for key, values in formed_elem.items():
-        key_elem = key.split("|")
-        http_stream_job_controller = {
-            "http_stream_job_controller_id": int(key_elem[0])
-        }
-        original_data = {}
-        for event in values:
-            if "HTTP_STREAM_JOB_CONTROLLER_PROXY_SERVER_RESOLVED" == event.event_type and original_data.get('proxy_server') is None:
-                original_data['proxy_server'] = event.other_data.get('params', {'proxy_server': "NONE"}).get('proxy_server')
-            if "HTTP_STREAM_JOB_CONTROLLER_BOUND" == event.event_type and original_data.get('url_request_id') is None:
-                original_data['url_request_id'] = event.source_dependency_id
-            if 'HTTP_STREAM_JOB_CONTROLLER' == event.event_type and 'PHASE_BEGIN' == event.phase and original_data.get("stream_start_time_int") is None:
-                original_data['stream_start_time_int'] = event.time_int
-        http_stream_job_controller['proxy_server'] = original_data.get('proxy_server', 'NONE')
-        http_stream_job_controller['url_request_id'] = original_data.get('url_request_id')
-        http_stream_job_controller['stream_start_time_int'] = original_data.get('stream_start_time_int')
-        http_stream_job_controller['original_data'] = original_data
-        http_stream_job_controllers.append(http_stream_job_controller)
-    return http_stream_job_controllers
 
 
 def process_http_stream_job(formed_elem):
@@ -208,94 +140,27 @@ def generate_general_info_files(general_infos, data_converted_path):
     general_info_files = []
     for general_info in general_infos:
         host = general_info['http_stream_job']['probe_url']
-        probe_session_starttime = general_info['http_stream_job_controller']['stream_start_time_int']
+        probe_session_starttime = general_info['quic_probe']['quic_session_starttime']
         general_info_file_path = '%s%s_%s_general_info.json' % (data_converted_path, host, probe_session_starttime)
         with open(general_info_file_path, "w") as gen_f:
-            json.dump(general_info,gen_f)
+            json.dump(general_info, gen_f)
         general_info_files.append(general_info_file_path)
         print("generate general_info file [%s]" % general_info_file_path)
     return general_infos
-
-
-# def process_quic_session(quic_formed_elem, data_converted_path):
-#     quics = []
-#     for key, values in quic_formed_elem.items():
-#         key_elem = key.split("|")
-#         quic = {
-#             "quic_session_id": int(key_elem[0])
-#         }
-#         host = "host"
-#         dns_begin_time = 0
-#         dns_end_time = 0
-#         for event in values:
-#             if 'QUIC_SESSION' == event.event_type and 'PHASE_BEGIN' in event.phase:
-#                 host = event.other_data.get('params').get('host')
-#         quic_connection = QuicConnection(host, dns_begin_time, dns_end_time, values, data_converted_path, "")
-#         json_obj = quic_connection.save()
-#         quic['original_datas'] = json_obj.get('general_info')
-#         quic['full_path_json_file'] = json_obj.get('fullpath_json_file')
-#         quic['quic_session_starttime'] = values[0].time_int
-#         quics.append(quic)
-#     return quics
-
-
-# def process_http2_session(http2_sessions, sockets):
-#     http2s = []
-#     for key, values in http2_sessions.items():
-#         key_elem = key.split("|")
-#         http2 = {
-#             "http2_session_id": int(key_elem[0])
-#         }
-#         original_data = {}
-#         for event in values:
-#             if 'HTTP2_SESSION_INITIALIZED' == event.event_type and 'PHASE_NONE' in event.phase:
-#                 dependency_socket_events = sockets.get("%s|%s" % (event.source_dependency_id, event.source_dependency_type), [])
-#                 for s_event in dependency_socket_events:
-#                     if 'TCP_CONNECT' == s_event.event_type and 'PHASE_BEGIN' in s_event.phase and original_data.get('http2_conn_establish_begin') is None:
-#                         original_data['http2_conn_establish_begin'] = s_event.time_int
-#                     if 'SSL_CONNECT' == s_event.event_type and 'PHASE_END' in s_event.phase and original_data.get('http2_conn_establish_end') is None:
-#                         original_data['http2_conn_establish_end'] = s_event.time_int
-#                 original_data['http2_conn_download_begin'] = event.time_int
-#             if 'HTTP2_SESSION_SEND_HEADERS' == event.event_type and 'PHASE_NONE' == event.phase:
-#                 original_data['http_job_stream_id'] = event.source_dependency_id
-#             original_data['http2_conn_download_end'] = event.time_int
-#         http2['http2_session_duration'] = original_data.get('http2_conn_establish_end', 0) - original_data.get('http2_conn_establish_begin', 0) + \
-#                                                   original_data.get('http2_conn_download_end', 0) - original_data.get('http2_conn_download_begin', 0)
-#         http2['http_job_stream_id'] = original_data['http_job_stream_id']
-#         http2['original_data'] = original_data
-#         http2s.append(http2)
-#     return http2s
 
 
 def process_netlog(fullpath, project_root, data_converted_path, filename_without_ext):
     print('begin to process log file')
     log_events = parse_netlog(fullpath, project_root)
     formed_log_events = get_formed_log_events(log_events)
-    # url_requests = process_url_request(formed_log_events[0])
-    # http_stream_job_controllers = process_http_stream_job_controller(formed_log_events[1])
     url_requests = probe_common.process_events(formed_log_events[0],probe_common.process_url_request)
     http_stream_job_controllers = probe_common.process_events(formed_log_events[1],probe_common.process_http_stream_job_controller)
     http_stream_jobs = process_http_stream_job(formed_log_events[2])
-    # quic_sessions = process_quic_session(formed_log_events[3], data_converted_path)
     quic_sessions = probe_quic.process_quic_session(formed_log_events[3], data_converted_path)
     http2_sessions = probe_http2.process_http2_session(formed_log_events[4], formed_log_events[6])
-    # general_infos, http2_session_durations = gen_general_infos(url_requests, http_stream_job_controllers, http_stream_jobs, quic_sessions, http2_sessions)
     general_infos = gen_general_infos(url_requests, http_stream_job_controllers, http_stream_jobs, quic_sessions, http2_sessions)
-    # for formed_elem in formed_log_events:
-    #     url_request_matrix = process_url_request(formed_elem)
-    #     for req_content_elem in formed_elem['REQUEST_CONTENT']:
-    #         general_info = {}
-    #         general_info.update(url_request_matrix)
-    #         general_info.update(process_http_stream_job_controller(req_content_elem))
-    #         general_info.update(process_http_stream_job(req_content_elem))
-    #         general_info.update(process_probe_data(data_converted_path, filename_without_ext, log_events))
-    #         general_infos.append(general_info)
-    # json_files = [x['json_file'] for x in general_infos]
     general_info_files = generate_general_info_files(general_infos, data_converted_path)
-    # json_files = process_probe_data(data_converted_path, filename_without_ext, log_events, general_infos)
     json_files = process_probe_data(data_converted_path, filename_without_ext, formed_log_events[-1], general_infos)
-    # json_files = []
-    # print("====", http2_session_durations)
     print('log file process finished')
     return json_files
 
@@ -344,28 +209,7 @@ def fix_truncated_file(file_path):
             print('no need to fix')
 
 
-#use first handshake message type to judge the log type
-# def get_netlog_type(events):
-#     for event in events:
-#         if event['type'] == 268:  # "QUIC_SESSION_CRYPTO_HANDSHAKE_MESSAGE_RECEIVED":268,
-#             return 'server'
-#         if event['type'] == 269:  # "QUIC_SESSION_CRYPTO_HANDSHAKE_MESSAGE_SENT":269,
-#             return 'client'
-
-
 def process_probe_data(data_converted_path, filename_without_ext, log_events, general_infos):
-    # log_type = get_netlog_type(log_events)
-    # print('netlog type is', log_type)
-    # start_time = int(log_events[0]['time'])
-    # if log_type == 'client':
-    #     quic_session = ClientQuicSession(start_time, data_converted_path, filename_without_ext)
-    # else:
-    #     quic_session = ServerQuicSession(start_time, data_converted_path, filename_without_ext)
-    # for log_event in log_events:
-    #     c_event = NetlogEvent(log_event)
-    #     quic_session.add_event(c_event)
-    # quic_session.save()
-    # json_files = quic_session.create_quic_connection()
     probe_quic.persist_event_list_to_csv(data_converted_path, filename_without_ext, log_events)
     json_files = []
     for info in general_infos:
